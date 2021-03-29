f711ffa9508b82c50957921fe57f58252354a1fc
devvv-iosss <81225988+devvv-iosss@users.noreply.github.com>
2021-03-29 08:20:29 +0300

    Сolor mixing changes

--- ColorViewChange_HW2.2/ViewController.swift
+++ ColorViewChange_HW2.2/ViewController.swift
@@ -2,18 +2,54 @@
 //  ViewController.swift
 //  ColorViewChange_HW2.2
 //
-//  Created by Anna on 26.03.2021.
+//  Created by dev on 26.03.2021.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabelValue: UILabel!
+    @IBOutlet var greenLabelValue: UILabel!
+    @IBOutlet var blueLabelValue: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 15
+       
+        redSlider.value = 0.1
+        greenSlider.value = 0.3
+        blueSlider.value = 0.5
+        
+        setColor()
+        changeOfValue()
     }
-
-
+   
+    @IBAction func rgbSliderAction() {
+        setColor()
+        changeOfValue()
+    }
+     
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func changeOfValue() {
+        redLabelValue.text = String(format: "%.2f", redSlider.value)
+        greenLabelValue.text = String(format:"%.2f", greenSlider.value)
+        blueLabelValue.text = String(format:"%.2f", blueSlider.value)
+    }
 }
 
